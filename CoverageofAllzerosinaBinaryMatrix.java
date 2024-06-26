a21f630173098572717c866c5e036e95513eb030
Divyanshi Maurya <116268950+Jiya003@users.noreply.github.com>
2024-06-26 20:06:03 +0530

    June 26 Day 1

---
+++ CoverageofAllzerosinaBinaryMatrix.java
@@ -0,0 +1,81 @@
+/*
+Coverage of all Zeros in a Binary Matrix
+Difficulty: EasyAccuracy: 55.68%Submissions: 18K+Points: 2
+
+Given a binary matrix contains 0s and 1s only, we need to find the sum of coverage of all zeros of the matrix where coverage for a particular 0 is defined as a total number of ones around a zero in left, right, up and bottom directions.
+
+Examples:
+
+Input:
+matrix = [[0, 1, 0],
+          [0, 1, 1],
+          [0, 0, 0]]
+Output: 6
+Explanation: There are a total of 6 coverage are there
+
+Input: 
+matrix = [[0, 1]]
+Output: 1
+Explanation: There are only 1 coverage.
+
+Expected Time Complexity: O(n * m)
+Expected Space Complexity: O(1)
+
+Constraints:
+1 <= matrix.size, matrix[0].size <= 100
+
+
+*/
+
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            int[][] matrix = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                String[] S = br.readLine().split(" ");
+                for (int j = 0; j < m; j++) {
+                    matrix[i][j] = Integer.parseInt(S[j]);
+                }
+            }
+            Solution ob = new Solution();
+            int ans = ob.findCoverage(matrix);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int findCoverage(int[][] matrix) {
+        // code here
+        int c=0;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(matrix[i][j]==0){
+                    if(i>0 && matrix[i-1][j]==1) c++;
+                    if(j>0 && matrix[i][j-1]==1) c++;
+                    if(i<matrix.length-1 && matrix[i+1][j]==1) c++;
+                    if(j<matrix[i].length-1 && matrix[i][j+1]==1) c++;
+                }
+            }
+        }
+        
+        return c;
+    }
+}
